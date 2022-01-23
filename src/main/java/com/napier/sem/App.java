822f0feadc62647bb7a99fabe7bdc6e9c40b3f28
jessica-magana <40526566@live.napier.ac.uk>
2022-01-23 16:40:04 -0600

    First commit, adding initial files.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -2,7 +2,7 @@ package com.napier.sem;
 
 public class App
 {
-    public static void main(String[] arguments)
+    public static void main(String[] args)
     {
         System.out.println("Boo yah!");
     }
